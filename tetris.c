1011c55afa006ea103fd2557cb181824f26c8048
Bastien <bastien.rinck@epitech.eu>
2017-02-20 12:21:39 +0100

    oui

---
+++ tetris.c
@@ -0,0 +1,14 @@
+/*
+** tetris.c for tetris in /home/rectoria/delivery/Projets/PSU_2016_tetris
+** 
+** Made by Bastien
+** Login   <rectoria@epitech.net>
+** 
+** Started on  Mon Feb 20 12:18:30 2017 Bastien
+** Last update Mon Feb 20 12:18:45 2017 Bastien
+*/
+
+int	main(int ac, char **av)
+{
+  return (0);
+}
