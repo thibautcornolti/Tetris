c49a3dedba919007c5d1d9bb68a5f7f2e6a35639
Bastien <bastien.rinck@epitech.eu>
2017-02-20 19:14:44 +0100

    mdr

--- tetrimino.c
+++ tetrimino.c
@@ -5,7 +5,7 @@
 ** Login   <rectoria@epi%tech.net>
 ** 
 ** Started on  Mon Feb 20 13:24:10 2017 Bastien
-** Last update Mon Feb 20 17:44:03 2017 Bastien
+** Last update Mon Feb 20 19:08:43 2017 Bastien
 */
 
 #include <sys/types.h>
@@ -15,74 +15,78 @@
 #include <unistd.h>
 #include "tetris.h"
 
-t_shapes	**put_structab(t_shapes **shapes, t_shapes *piece, int size)
+void		put_structab(t_shapes ***shapes, t_shapes *piece, int size)
 {
   t_shapes	**tab;
   int		i;
 
-  i = (!shapes) ? 0 : -1;
+  i = (!*shapes) ? 0 : -1;
   if ((tab = malloc(sizeof(t_shapes *) * (size + 1))) == NULL)
-    return (NULL);
-  while (shapes && shapes[++i])
-    tab[i] = shapes[i];
+    return ;
+  while ((*shapes) && (*shapes)[++i])
+    tab[i] = (*shapes)[i];
   tab[i] = piece;
-  /* free (*shapes); */
   tab[i + 1] = NULL;
-  return (tab);
+  free(*shapes);
+  (*shapes) = tab;
 }
 
-void	get_size(t_shapes *shape, char *first_line)
+void	get_size(t_shapes *piece, char *first_line)
 {
   char	**tab;
   char	*str;
 
   tab = my_strsplit(first_line, ' ');
-  shape->width = my_getnbr(tab[0]);
-  shape->height = my_getnbr(tab[1]);
-  shape->color = my_getnbr(tab[2]);
-  /* free_tab(tab); */
+  piece->width = my_getnbr(tab[0]);
+  piece->height = my_getnbr(tab[1]);
+  piece->color = my_getnbr(tab[2]);
+  free_tab(&tab);
 }
 
-t_shapes	**add_shape(t_shapes **shapes, int fd)
+void	add_shape(t_shapes ***shapes, int fd)
 {
   t_shapes	*piece;
   char		*str;
   char		*temp;
+  char		*antileak;
   static int	size = 1;
 
   str = NULL;
   if ((piece = malloc(sizeof(t_shapes))) == NULL)
-    return NULL;
+    return ;
   get_size(piece, get_next_line(fd));
   while ((temp = get_next_line(fd)) != NULL)
-    str = my_strmcat(my_strmcat(str, "@"), temp);
+    {
+      antileak = my_strmcat(str, "@");
+      free(str);
+      str = my_strmcat(antileak, temp);
+      free(temp);
+      free(antileak);
+    }
   piece->map = my_strsplit(str, '@');
-  shapes = put_structab(shapes, piece, size);
+  put_structab(shapes, piece, size);
+  free(str);
   size += 1;
-  return (shapes);
 }
 
-t_shapes	**get_tetrimino()
+void		get_tetrimino(t_shapes ***shapes)
 {
   DIR		*directory;
   struct dirent	*file;
   int		fd;
-  t_shapes	**shapes;
 
-  shapes = NULL;
   directory = opendir("./tetriminoes");
   while ((file = readdir(directory)) != NULL)
     {
       if (file->d_name[0] != '.')
 	{
 	  if ((fd = open(my_strmcat("./tetriminoes/", file->d_name), O_RDONLY)) == -1)
-	    return (NULL);
-	  shapes = add_shape(shapes, fd);
+	    return ;
+	  add_shape(shapes, fd);
 	}
       close(fd);
     }
   closedir(directory);
-  return (shapes);
 }
 
 int	main()
@@ -90,13 +94,20 @@ int	main()
   t_shapes	**tab;
   int		i;
   int		k;
-
+  
   i = -1;
-  tab = get_tetrimino();
+  tab = NULL;
+  get_tetrimino(&tab);
   while (tab && tab[++i])
     {
       k = -1;
       while (tab[i]->map[++k])
 	printf("%s\n", tab[i]->map[k]);
     }
+  while (tab && --i >= 0)
+    {
+      free_tab(&tab[i]->map);
+      free(tab[i]);
+    }
+  free(tab);
 }
