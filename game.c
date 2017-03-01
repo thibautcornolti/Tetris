321a64dead828147ce600b3b142fbeb7a97237eb
Bastien <bastien.rinck@epitech.eu>
2017-03-01 20:04:49 +0100

    la bonne boucle inf

--- game.c
+++ game.c
@@ -5,7 +5,7 @@
 ** Login   <rectoria@epitech.net>
 ** 
 ** Started on  Tue Feb 28 16:42:04 2017 Bastien
-** Last update Wed Mar  1 17:38:41 2017 Bastien
+** Last update Wed Mar  1 20:03:59 2017 Bastien
 */
 
 #include <stdlib.h>
@@ -29,12 +29,12 @@ void	init_board(char ***board, t_pars *pars)
   int	j;
 
   i = -1;
-  if ((*board = malloc(sizeof(char *) * (pars->row + 1))) == NULL)
+  if (((*board) = malloc(sizeof(char *) * (pars->row + 1))) == NULL)
     return ;
   while (++i < pars->row)
     {
       j = -1;
-      if ((*board[i] = malloc(sizeof(char) * (pars->col + 1))) == NULL)
+      if (((*board)[i] = malloc(sizeof(char) * (pars->col + 1))) == NULL)
 	return ;
       while (++j < pars->col)
 	(*board)[i][j] = -1;
@@ -69,11 +69,16 @@ void	falling_shapes(char **board, t_pos *pos)
     {
       j = -1;
       while (pos->map[i][++j])
-	if (pos->map[i][j] > 0 && board[pos->y + 1 + i][pos->x + j] > 0)
+	if (!board[pos->y + i + 1] && !pos->map[i + 1])
 	  {
 	    pos->index = -1;
 	    return ;
 	  }
+      if (pos->map[i][j] > 0 && board[pos->y + i][pos->x + j] > 0)
+	{
+	  pos->index = -1;
+	  return ;
+	}
     }
   pos->y += 1;
 }
@@ -81,18 +86,19 @@ void	falling_shapes(char **board, t_pos *pos)
 void	game(t_shapes *shapes, t_pars *pars, t_game *game)
 {
   char	**board;
-  t_pos	*pos;
+  t_pos	pos;
   int	action;
 
   init_board(&board, pars);
   while (check_loss(board))
     {
-      rand_next(pos, shapes, pars);
-      while (pos->index != -1)
+      rand_next(&pos, shapes, pars);
+      while (&pos.index != -1)
 	{
 	  if ((action = get_action(pars)))
-	    apply_action(action, board, pos, shapes);
-	  falling_shapes(board, pos);
+	    apply_action(action, board, &pos, shapes);
+	  printf("oui");
+	  falling_shapes(board, &pos);
 	  //	  display(board, pos, pars);
 	}
     }
